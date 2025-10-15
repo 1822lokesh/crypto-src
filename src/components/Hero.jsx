25b7b3af70dbb3c55414a1d3a3d548fe36fc0cf5
1822lokesh <lokeshharikant4@gmail.com>
2025-10-15 12:36:32 +0530

    added tab sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,63 +1,129 @@
 import * as React from "react";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Box from "@mui/material/Box";
+import {
+  Box,
+  Typography,
+  FormControl,
+  Select,
+  MenuItem,
+  Tabs,
+  Tab,
+  IconButton,
+} from "@mui/material";
+import { Settings, KeyboardArrowUp, Refresh } from "@mui/icons-material";
 
 export default function Hero() {
   const [screen, setScreen] = React.useState("");
   const [universe, setUniverse] = React.useState("");
+  const [tab, setTab] = React.useState(0);
 
-  const handleScreenChange = (event) => {
-    setScreen(event.target.value);
-  };
-
-  const handleUniverseChange = (event) => {
-    setUniverse(event.target.value);
-  };
+  const handleScreenChange = (event) => setScreen(event.target.value);
+  const handleUniverseChange = (event) => setUniverse(event.target.value);
+  const handleTabChange = (event, newValue) => setTab(newValue);
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column" 
-      alignItems="flex-start" 
-      sx={{ m: 2 }}
-    >
-      {/* Universe Dropdown */}
-      <FormControl sx={{ mb: 2, minWidth: 180 }}>
-        <Select
-          value={universe}
-          onChange={handleUniverseChange}
-          displayEmpty
-          inputProps={{ "aria-label": "Universe Selector" }}
-        >
-          <MenuItem value="">
-            <em>Select Universe</em>
-          </MenuItem>
-          <MenuItem value={1}>Crypto Universe</MenuItem>
-          <MenuItem value={2}>ETF Universe</MenuItem>
-          <MenuItem value={3}>Bond Universe</MenuItem>
-        </Select>
-      </FormControl>
+    <Box sx={{ p: 3 }}>
+      {/* Title */}
+      {/* <Typography variant="h5" fontWeight="bold" mb={2}>
+        Crypto universe
+      </Typography> */}
 
-      {/* Screener Dropdown (below) */}
-      <FormControl sx={{ minWidth: 250 }}>
-        <Select
-          value={screen}
-          onChange={handleScreenChange}
-          displayEmpty
-          inputProps={{ "aria-label": "Screener Selector" }}
+      {/* Universe + Screener Dropdowns */}
+      <Box display="flex" flexDirection="column" alignItems="flex-start" mb={3}>
+        {/* Universe Dropdown */}
+        <FormControl sx={{ mb: 2, minWidth: 200 }}>
+          <Select
+            value={universe}
+            onChange={handleUniverseChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Universe Selector" }}
+          >
+            <MenuItem value="">
+              <em>Select Universe</em>
+            </MenuItem>
+            <MenuItem value={1}>Crypto Universe</MenuItem>
+            <MenuItem value={2}>ETF Universe</MenuItem>
+            <MenuItem value={3}>Bond Universe</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/* Screener Dropdown (below Universe) */}
+        <FormControl sx={{ minWidth: 250 }}>
+          <Select
+            value={screen}
+            onChange={handleScreenChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Screener Selector" }}
+          >
+            <MenuItem value="">
+              <em>Select Screener</em>
+            </MenuItem>
+            <MenuItem value={1}>Stock Screener</MenuItem>
+            <MenuItem value={2}>ETF Screener</MenuItem>
+            <MenuItem value={3}>Bond Screener</MenuItem>
+            <MenuItem value={4}>Crypto Coins Screener</MenuItem>
+            <MenuItem value={5}>CEX Screener</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      {/* Filter Row */}
+      <Box display="flex" flexWrap="wrap" gap={1.5} alignItems="center" mb={3}>
+        {[
+          "Watchlist",
+          "Rank",
+          "Market cap",
+          "FD market cap",
+          "Vol / Market Cap",
+          "Addr with balance",
+          "Volume",
+          "Vol Change %",
+          "Tx vol in USD",
+        ].map((label, index) => (
+          <FormControl key={index} size="small" sx={{ minWidth: 160 }}>
+            <Select displayEmpty value="">
+              <MenuItem value="">
+                <em>{label}</em>
+              </MenuItem>
+            </Select>
+          </FormControl>
+        ))}
+
+        <IconButton>
+          <Settings />
+        </IconButton>
+        <IconButton>
+          <KeyboardArrowUp />
+        </IconButton>
+      </Box>
+
+      {/* Tabs Section */}
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Tabs
+          value={tab}
+          onChange={handleTabChange}
+          textColor="inherit"
+          indicatorColor="primary"
+          sx={{
+            "& .MuiTab-root": { textTransform: "none", fontWeight: 500 },
+          }}
         >
-          <MenuItem value="">
-            <em>Select Screener</em>
-          </MenuItem>
-          <MenuItem value={1}>Stock Screener</MenuItem>
-          <MenuItem value={2}>ETF Screener</MenuItem>
-          <MenuItem value={3}>Bond Screener</MenuItem>
-          <MenuItem value={4}>Crypto Coins Screener</MenuItem>
-          <MenuItem value={5}>CEX Screener</MenuItem>
-        </Select>
-      </FormControl>
+          {[
+            "Overview",
+            "Performance",
+            "Valuation",
+            "Addresses",
+            "Transactions",
+            "Sentiment",
+            "Technicals",
+          ].map((label) => (
+            <Tab key={label} label={label} />
+          ))}
+        </Tabs>
+
+        <IconButton>
+          <Refresh />
+        </IconButton>
+      </Box>
     </Box>
   );
 }
